5fe0e61bb658b1055e11796ab018a2936c3fa768
Fran Moreno <franches9511@gmail.com>
2021-07-31 18:33:45 +0200

    mejora_imagenes

--- js/juego.js
+++ js/juego.js
@@ -6,54 +6,45 @@ var FPS = 50;
 var altoF = 50;
 var anchoF = 50;
 
+var tileMap;
 
-var pared = "#3d9a25";
+/*var pared = "#3d9a25";
 var tierra = '#804000';
 
 
 //var jugador = "#FF0000";
 var llave = '#ffff00';
 var puerta = '#8040c0';
-
+*/
 
 
 var tablero = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 3, 0],
-    [0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0],
-    [0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0],
-    [0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0],
-    [0, 0, 2, 0, 1, 1, 1, 1, 1, 1, 0, 0],
-    [0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0],
-    [0, 1, 1, 1, 0, 0, 0, 0, 1, 2, 0, 0],
-    [0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0],
-    [0, 1, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 2, 2, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+    [0, 0, 2, 2, 0, 0, 0, 0, 0, 2, 2, 0],
+    [0, 0, 0, 2, 2, 0, 0, 0, 0, 2, 0, 0],
+    [0, 0, 0, 0, 2, 2, 0, 0, 0, 2, 0, 0],
+    [0, 0, 3, 0, 2, 2, 2, 2, 2, 2, 0, 0],
+    [0, 0, 2, 0, 2, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 2, 2, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 0, 0],
+    [0, 2, 2, 2, 0, 0, 0, 0, 2, 3, 0, 0],
+    [0, 2, 2, 0, 0, 0, 0, 0, 2, 2, 0, 0],
+    [0, 2, 3, 2, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 ];
+
+
+
 
 function dibujarEscenario() {
     var color;
 
     for (y = 0; y < tablero.length; y++) {
         for (x = 0; x < tablero[0].length; x++) {
-            if (tablero[y][x] == 0) {
-                color = pared;
-            }
-            if (tablero[y][x] == 1) {
-                color = tierra;
-            }
-            if (tablero[y][x] == 2) {
-                color = llave;
-            }
-            if (tablero[y][x] == 3) {
-                color = puerta;
-            }
-
-            ctx.fillStyle = color;
-            ctx.fillRect(x * anchoF, y * altoF, anchoF, altoF);
+            var tile = tablero[y][x];
+            ctx.drawImage(tileMap, tile * 32, 0, 32, 32, anchoF * x, altoF * y, anchoF, altoF);
         }
     }
 }
@@ -66,85 +57,81 @@ function personaje() {
     this.color = '#FF0000';
     var contadorLlaves = 0;
     this.pintar = function () {
-    var door = 0;
-        ctx.fillStyle = this.color;
-        ctx.fillRect(this.x * anchoF, this.y * altoF, anchoF, altoF);
+        var door = 0;
+        ctx.drawImage(tileMap, 32, 32, 32, 32, this.x * anchoF, this.y * altoF, anchoF, altoF);
+
 
     }
-    this.margenes = function(x,y){
+    this.margenes = function (x, y) {
         var colision = false;
-        if (tablero[y][x] == 0){
+        if (tablero[y][x] == 0) {
             colision = true;
         }
         return (colision);
     }
 
-    this.AlmacenarLlave = function(x,y,z){
-        if (tablero[y][x] == 2){
+    this.AlmacenarLlave = function (x, y, z) {
+        if (tablero[y][x] == 3) {
             z++;
-            tablero[y][x] =1;
-    }
+            tablero[y][x] = 2;
+        }
         return (z);
     }
-    this.reiniciar = function(){
+    this.reiniciar = function () {
         this.x = 1;
-        this.y =1;
+        this.y = 1;
         contadorLlaves = 0;
         door = 0;
-        tablero[5][2] = 2;
-        tablero[10][9] = 2;
-        tablero[12][2] = 2;
-        tablero[1][10] = 3;
+        tablero[5][2] = 3;
+        tablero[10][9] = 3;
+        tablero[12][2] = 3;
     }
-    this.door = function(x,y,contadorLlaves){
-        if (tablero[y][x] == 3){
-            if(contadorLlaves ==3){
+    this.door = function (x, y, contadorLlaves) {
+        if (tablero[y][x] == 1) {
+            if (contadorLlaves == 3) {
                 this.reiniciar();
                 console.log("Has ganado");
 
-            }
-            else{
-                if(contadorLlaves == 2){
-                console.log("Te falta " + (3-contadorLlaves) + " llave");
-                }
-                else 
-                console.log("Te faltan " + (3-contadorLlaves) + " llaves");
+            } else {
+                if (contadorLlaves == 2) {
+                    console.log("Te falta " + (3 - contadorLlaves) + " llave");
+                } else
+                    console.log("Te faltan " + (3 - contadorLlaves) + " llaves");
 
+            }
+        }
     }
-}
-}
     this.mueveArriba = function () {
-         if (this.margenes(this.x ,this.y-1) == false){
+        if (this.margenes(this.x, this.y - 1) == false) {
             this.y--;
-            contadorLlaves = this.AlmacenarLlave(this.x,this.y,contadorLlaves);
-            door = this.door(this.x,this.y,contadorLlaves);
+            contadorLlaves = this.AlmacenarLlave(this.x, this.y, contadorLlaves);
+            door = this.door(this.x, this.y, contadorLlaves);
 
         }
     }
     this.mueveAbajo = function () {
-        if (this.margenes(this.x,this.y+1) == false) {
+        if (this.margenes(this.x, this.y + 1) == false) {
 
             this.y++;
-            contadorLlaves = this.AlmacenarLlave(this.x,this.y,contadorLlaves);
-            door = this.door(this.x,this.y,contadorLlaves);
+            contadorLlaves = this.AlmacenarLlave(this.x, this.y, contadorLlaves);
+            door = this.door(this.x, this.y, contadorLlaves);
 
         }
     }
     this.mueveIzquierda = function () {
-        if (this.margenes(this.x-1,this.y) == false) {
+        if (this.margenes(this.x - 1, this.y) == false) {
 
             this.x--;
-            contadorLlaves = this.AlmacenarLlave(this.x,this.y,contadorLlaves);
-            door = this.door(this.x,this.y,contadorLlaves);
-
+            contadorLlaves = this.AlmacenarLlave(this.x, this.y, contadorLlaves);
+            door = this.door(this.x, this.y, contadorLlaves);
         }
     }
     this.mueveDerecha = function () {
-        if (this.margenes(this.x+1,this.y) == false) {
+        if (this.margenes(this.x + 1, this.y) == false) {
 
             this.x++;
-            contadorLlaves = this.AlmacenarLlave(this.x,this.y,contadorLlaves);
-            door = this.door(this.x,this.y,contadorLlaves);
+            contadorLlaves = this.AlmacenarLlave(this.x, this.y, contadorLlaves);
+            door = this.door(this.x, this.y, contadorLlaves);
 
         }
 
@@ -156,6 +143,9 @@ function inicializar() {
 
     canvas = document.getElementById('mycanvas');
     ctx = canvas.getContext('2d');
+
+    tileMap = new Image();
+    tileMap.src = 'img/tilemap.png';
 
     protagonista = new personaje();
 
